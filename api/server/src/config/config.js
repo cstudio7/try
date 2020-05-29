135299461cfdc1d61a51747c3f4df94d972112d2
Victor Godwin <conquerorsword36@yahoo.com>
2020-05-29 16:22:23 -0700

    server setup1

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -4,17 +4,17 @@ require('dotenv').config();
 module.exports = {
 
   // If using onine database
-  // development: {
-  //   use_env_variable: 'DATABASE_URL'
-  // },
-
   development: {
-    database: 'books',
-    username: 'me',
-    password: 'password',
-    host: '127.0.0.1',
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_URL'
   },
+
+  // development: {
+  //   database: 'books',
+  //   username: 'me',
+  //   password: 'password',
+  //   host: '127.0.0.1',
+  //   dialect: 'postgres'
+  // },
 
   test: {
     database: 'book_test',
